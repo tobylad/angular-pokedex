8ccff95a99fa5d1c77982d7fb911936eaf73f762
Toby Ladislas <tladislas@gmail.com>
2020-07-16 14:45:12 -0400

    Add pokemon-list-item component to get more information on the list screen.

---
+++ src/app/pokemon-list/pokemon-list-item/pokemon-list-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PokemonService } from 'src/app/pokemon.service';
+import { Observable } from 'rxjs';
+import { IPokemon } from 'src/app/ipokemon.interface';
+
+@Component({
+  selector: 'app-pokemon-list-item',
+  templateUrl: './pokemon-list-item.component.html',
+  styleUrls: ['./pokemon-list-item.component.css']
+})
+export class PokemonListItemComponent implements OnInit {
+  @Input() pokemonName: string;
+
+  public pokemon$: Observable<IPokemon>;
+
+  constructor(
+    private pokemonService: PokemonService
+  ) { }
+
+  ngOnInit(): void {
+    this.pokemon$ = this.pokemonService.getPokemonByName(this.pokemonName);
+  }
+
+}
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,52 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { PokemonService } from '../pokemon.service';
 import { IPokemon } from '../ipokemon.interface';
-import { createViewChild } from '@angular/compiler/src/core';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-pokemon-list',
-  templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+	selector: 'app-pokemon-list',
+	templateUrl: './pokemon-list.component.html',
+	styleUrls: ['./pokemon-list.component.css']
 })
 export class PokemonListComponent implements OnInit {
-  public allPokemon: IPokemon[];
-  public allPokemon$: Observable<IPokemon[]>;
-  public name: string;
-  public selectedId: number;
+	public allPokemon: IPokemon[];
 
-  constructor(
-    private route: ActivatedRoute,
-    private pokemonService: PokemonService
-  ) {}
+	constructor(
+		private pokemonService: PokemonService
+	) {}
 
-  public ngOnInit(): void {
-    this.getPokemon();
+	public ngOnInit(): void {
+		this.getPokemon();
+	}
 
-    this.allPokemon$ = this.route.paramMap.pipe(
-      switchMap(params => {
-        this.selectedId = Number(params.get("id"));
-        return this.pokemonService.getAllPokemon();
-      })
-    )
+	private getPokemon(): void {
+		const pokemon = this.pokemonService.getAllPokemon();
 
-    this.route.queryParams.subscribe(params => {
-      this.name = params['name'];
-    });
-
-    console.log("%cthis.name", "color: ; font-size: 16px;", this.name)
-  }
-
-  private getPokemon(): void {
-    const pokemon = this.pokemonService.getAllPokemon();
-
-    pokemon.subscribe((data: any) => {
-      this.allPokemon = data.results;
-      console.log(data);
-    })
-
-  }
-
+		pokemon.subscribe((data) => {
+			this.allPokemon = data.results;
+		})
+	}
+	
 }
